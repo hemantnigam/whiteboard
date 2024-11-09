3124f2a5ef899b7b125b17957e73e6dedadc940f
Hemant Nigam <hnigam@chegg.com>
2024-11-09 07:16:59 +0530

    update cors config

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
     origin: "https://resonant-kataifi-726e09.netlify.app/",
-    methods: ["GET", "POST"],
+    credentials: true
   },
 });
 
